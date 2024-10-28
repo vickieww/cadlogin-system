a1f6d54ced8852da390df5c60c62bf297f02a67a
vickieww <gabrieli.vitoria@alunoescolassociais.org.br>
2024-10-28 07:53:01 -0300

    Exclui link de referência do código de estilização da página das listas dos usuários

--- views/list_users.php
+++ views/list_users.php
@@ -10,7 +10,6 @@ if(isset($_SESSION['perfil'])):
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuários</title>
-    <link rel="stylesheet" type='text/css' media='screen' href="css/list.css"> 
     <style>
        body {
     background-color: #FDDDE6;
